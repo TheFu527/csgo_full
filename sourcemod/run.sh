92277538ef6e95a1579101a3ecc0b1482e7dafc9
huoji120 <1296564236@qq.com>
2019-08-23 22:15:30 +0800

    增加服务端插件

--- sourcemod/run.sh
+++ sourcemod/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./srcds_run -game csgo -console -tickrate 128 +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount 0A946080584B114081FF040B7F82F60A -net_port_try 1 -nomaster +sv_occlude_players 1 +pvs_min_player_Distance 1 +sv_mincmdrate 128 +sv_minupdaterate 128
+./srcds_run -game csgo -console -tickrate 128 +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount XXXX -net_port_try 1 -nomaster +sv_occlude_players 1 +pvs_min_player_Distance 1 +sv_mincmdrate 128 +sv_minupdaterate 128
 
